ae010bbfc49043f44fe3cc8b3a287049c007b970
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-09-02 11:35:12 +0200

    Add TODO

--- maplibre/__init__.py
+++ maplibre/__init__.py
@@ -3,6 +3,8 @@ from .controls import ControlPosition, ControlType
 from .layer import Layer, LayerType
 from .map import Map, MapOptions
 from .mapcontext import MapContext
+
+# TODO: Only import once, preferred: MapLibreRenderer
 from .renderer import MapLibreRenderer
 from .renderer import MapLibreRenderer as render_maplibregl
 from .ui import output_maplibregl
