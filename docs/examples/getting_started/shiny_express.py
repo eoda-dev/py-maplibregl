359240f9835c841a4e2f6c14e9410486d513e2ee
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-12-17 08:40:52 +0100

    Update example

--- docs/examples/getting_started/shiny_express.py
+++ docs/examples/getting_started/shiny_express.py
@@ -1,8 +1,10 @@
 import json
 
+from shiny import reactive
 from shiny.express import input, render, ui
 
-from maplibre import Map, MapOptions, render_maplibregl
+from maplibre import Map, MapContext, MapOptions, render_maplibregl
+from maplibre.controls import Marker
 
 ui.h1("My awesome MapLibre map")
 
@@ -26,3 +28,10 @@ ui.div("Move map to change view state.")
 @render.code
 def view_state():
     return json.dumps(input.mapgl_view_state(), indent=2)
+
+
+@reactive.Effect
+@reactive.event(input.mapgl_clicked)
+async def set_marker():
+    async with MapContext("mapgl") as m:
+        m.add_marker(Marker(lng_lat=input.mapgl_clicked()["coords"].values()))
