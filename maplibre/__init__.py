cc1c1f398266ecca62975bca233c8f1ea9ed85c7
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-04-04 09:57:08 +0200

    Make shiny optional

--- maplibre/__init__.py
+++ maplibre/__init__.py
@@ -2,9 +2,14 @@ from .config import options
 from .controls import ControlPosition, ControlType
 from .layer import Layer, LayerType
 from .map import Map, MapOptions
-from .mapcontext import MapContext
 
-# TODO: Only import once, preferred: MapLibreRenderer
-from .renderer import MapLibreRenderer
-from .renderer import MapLibreRenderer as render_maplibregl
-from .ui import output_maplibregl
+try:
+    from .mapcontext import MapContext
+    from .renderer import MapLibreRenderer
+
+    # from .renderer import MapLibreRenderer as render_maplibregl
+    from .ui import output_maplibregl
+
+    render_maplibregl = MapLibreRenderer
+except ImportError as e:
+    pass
