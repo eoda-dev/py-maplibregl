64bce383c47f4e3a843e0af73a8a52d382363ae6
Stefan Kuethe <stefan.kuethe@eoda.de>
2025-04-09 23:33:11 +0200

    Refactor

--- examples/basemaps/raster-tiles.py
+++ examples/basemaps/raster-tiles.py
@@ -7,17 +7,9 @@ from maplibre.basemaps import construct_basemap_style
 style = construct_basemap_style(
     sources={
         "raster-tiles": RasterSource(
-            tiles=[
-                # "https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg",
-                "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            ],
+            tiles=["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
             tile_size=256,
             attribution="&copy; OpenStreetMap Contributors",
-            # (
-            #    'Map tiles by <a target="_blank" href="https://stamen.com">Stamen Design</a>;'
-            #    ' Hosting by <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>. Data &copy;'
-            #    ' <a href="https://www.openstreetmap.org/about" target="_blank">OpenStreetMap</a> contributors'
-            # ),
         )
     },
     layers=[
