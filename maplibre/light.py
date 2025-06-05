2677ff45aa5be1ae8f48b48a3be43e144f96b2d7
Konrad <Konkrad@users.noreply.github.com>
2025-06-05 11:35:01 +0200

    Update light.py

--- maplibre/light.py
+++ maplibre/light.py
@@ -14,6 +14,6 @@ class Light(MapLibreBaseModel):
     """
 
     anchor: Literal["map", "viewport"] = "map"
-    position: list[float, float, float] = [1.15, 210, 30]
+    position: list[float] = [1.15, 210, 30]
     color: str = "#ffffff"
     intensity: float = Field(0.5, ge=0, le=1)
